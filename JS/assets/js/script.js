d25ca26f5e30b3f25f2fc2236d16a826ea252614
jhonjonh <john.gerber@live.com>
2022-01-24 12:57:42 -0300

    ajustes e desafios

--- JS/assets/js/script.js
+++ JS/assets/js/script.js
@@ -1,11 +1,39 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
+var subtrair = document.querySelector("[name='subtrair']");
+var adicionar = document.querySelector("[name='adicionar']");
+
+adicionar.addEventListener('click', increment);
+subtrair.addEventListener('click', decrement);
 
 function increment() {
-    currentNumber ++;
-    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber >= 10) {
+        adicionar.disabled = true;
+    } else {
+        subtrair.disabled = false;
+        currentNumber ++;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } 
+    avermelhou();
 }
-function decrement() {
-    currentNumber --;
-    currentNumberWrapper.innerHTML = currentNumber;
+
+function decrement() {  
+    if(currentNumber <= (-10)) {
+        subtrair.disabled = true;
+    } else {
+        adicionar.disabled = false;
+        currentNumber --;
+        currentNumberWrapper.innerHTML = currentNumber;
+    } 
+    avermelhou();
 }
+
+function avermelhou() {
+    if (currentNumber < 0) {
+        currentNumberWrapper.style.color = "red";
+    } else {
+        currentNumberWrapper.style.color = "black";
+    }
+}
+
+
