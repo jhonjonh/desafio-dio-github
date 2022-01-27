54834a14f1b6b63039ecd50bc6723278acc7e8b6
jhonjonh <john.gerber@live.com>
2022-01-27 11:29:20 -0300

    exerc sintaxe

---
+++ JS/Sintaxe/nomecompleto.js
@@ -0,0 +1,12 @@
+let firstName = "Jogusso";
+let lastName = "Gurlandez";
+const FULL_NAME = `${firstName} ${lastName}`;
+lastName = "Puripuri"
+console.log(`${FULL_NAME}`);
+
+let palavra = new String("nueme");
+console.log(typeof(palavra));
+
+let numero = new Number(5);
+
+
